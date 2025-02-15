eba9275771000c2822f8c9c21f3aff4480425c0a
bharath-chen <sscbarath@gmail.com>
2025-02-15 21:22:27 +0530

    removed comments and added vercel.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-// import "owl.carousel/dist/assets/owl.carousel.css";
-// import "owl.carousel/dist/assets/owl.theme.default.css";
 import "swiper/css";
 import "swiper/css/navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
